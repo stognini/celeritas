dd9742e9fcc761c11a4e4853e2d630ea3ea7280f
Stefano Tognini <togninis@ornl.gov>
2026-02-13 16:07:51 -0500

    Sketch (incomplete) macro_xs

--- src/celeritas/mucf/process/MucfProcess.cc
+++ src/celeritas/mucf/process/MucfProcess.cc
@@ -9,6 +9,7 @@
 #include <memory>
 
 #include "celeritas/mat/MaterialParams.hh"
+#include "celeritas/mucf/Types.hh"
 #include "celeritas/mucf/model/DTMixMucfModel.hh"
 #include "celeritas/phys/Model.hh"
 #include "celeritas/phys/ParticleParams.hh"
@@ -47,8 +48,45 @@ auto MucfProcess::macro_xs(Applicability applic) const -> XsGrid
     auto const& data = model_->host_ref();
     CELER_ASSERT(data);
 
-    XsGrid result;
+    auto find_mucf_matid
+        = [](PhysMatId matid, auto const& mucfmatid_to_matid) -> MuCfMatId {
+        for (auto id : range(mucfmatid_to_matid.size()))
+        {
+            MuCfMatId mucf_matid{id};
+            if (mucfmatid_to_matid[mucf_matid] == matid)
+            {
+                return mucf_matid;
+            }
+        }
+        return {};
+    };
 
+    auto mucf_matid = find_mucf_matid(applic.material, data.mucfmatid_to_matid);
+    if (!mucf_matid)
+    {
+        // Not a muCF material
+        return {};
+    }
+
+    // Get total material cycle time
+    auto const& mat_cycle_times = data.cycle_times[mucf_matid];
+    real_type total_cycle_times{0};
+    for (auto mol : range(MucfMuonicMolecule::size_))
+    {
+        for (auto spin : range(2))
+        {
+            total_cycle_times += mat_cycle_times[mol][spin];
+        }
+    }
+
+    real_type cycle_rate = real_type{1} / total_cycle_times;  // [1/s]
+
+    // Constant "XS" grid with constant cycle rates
+    XsGrid result;
+    result.lower.x[Bound::lo] = applic.lower.value();
+    result.upper.x[Bound::hi] = applic.upper.value();
+    result.lower.y = {cycle_rate, cycle_rate};
+    result.upper.y = {cycle_rate, cycle_rate};
     return result;
 }
 
